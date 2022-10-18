738faf57c0b81975df85ce5f9244ac7bb816bb87
anitha14 <Anitha.tumarada@gmail.com>
2022-10-18 10:24:33 -0400

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,24 @@
+/**09-06-2022
+*Copyright 2022 Howard Community College
+*Anitha Tumarada
+*Version 1.0
+*/
+
+//This is the public class declaration name Output
+public class Output {
+	
+	//This is the beginning of the main function
+	public static void main(String [] args) {
+		System.out.println("Copyright 2022 Howard Community College"); //Copyright message
+		System.out.println();
+		System.out.println("The println method prints to the screen and then goes to the next line");
+		System.out.print("Print stays on the current line.\n");
+		System.out.print("unless you force it to the next line.\n");
+		System.out.print("This string tests the tab sequence.\t\n"); 
+		System.out.print("\"This string prints a \"character\n");	
+		System.out.printf("This is a test of the printf output.\n");
+		System.out.printf("This prints an integer value: 10\n");
+		System.out.printf("This prints a floating point value: 3.14\n");
+		
+	} //End of main function
+} //End of Output class
